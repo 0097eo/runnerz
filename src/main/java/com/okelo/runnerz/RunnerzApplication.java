0b41503bc30d1ddd8a3bd49e61fd429a67ba4977
0097eo <emmanuelokello294@gmail.com>
2025-05-12 10:28:48 +0300

    add logging and CommandLineRunner for application startup

--- src/main/java/com/okelo/runnerz/RunnerzApplication.java
+++ src/main/java/com/okelo/runnerz/RunnerzApplication.java
@@ -1,13 +1,39 @@
 package com.okelo.runnerz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.okelo.runnerz.run.Location;
+import com.okelo.runnerz.run.Run;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class RunnerzApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
+		log.info("Application started successfully");
+	}
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			//log.info("CommandLineRunner executed");
+
+			Run run = new Run(
+					1,
+					"Morning Run",
+					LocalDateTime.now(),
+					LocalDateTime.now().plusHours(1),
+					5000,
+					Location.OUTDOOR
+			);
+			log.info("Run created: {}", run);
+		};
 	}
 
 }
---
+++ src/main/java/com/okelo/runnerz/run/Location.java
@@ -0,0 +1,5 @@
+package com.okelo.runnerz.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}

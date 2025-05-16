2c891a64e1621cc9840c69b23966514a60936be0
0097eo <emmanuelokello294@gmail.com>
2025-05-16 10:46:15 +0300

    refactor RunRepository to convert to interface and define CRUD operations

--- src/main/java/com/okelo/runnerz/run/RunRepository.java
+++ src/main/java/com/okelo/runnerz/run/RunRepository.java
@@ -1,45 +1,23 @@
 package com.okelo.runnerz.run;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class RunRepository {
-    private List<Run> runs = new ArrayList<>();
+public interface RunRepository {
 
-    List<Run> findAll() {
-        return runs;
-    }
+    List<Run> findAll();
 
-    Optional<Run> findById(Integer id) {
-        return runs.stream()
-                .filter(run -> run.id().equals(id))
-                .findFirst();
-    }
+    Optional<Run> findById(Integer id);
 
-    void create(Run run) {
-        runs.add(run);
-    }
+    void create(Run run);
 
-    void update(Run run, Integer id){
-        Optional<Run> existingRun = findById(id);
-        existingRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
-    }
+    void update(Run run, Integer id);
 
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
-    }
+    void delete(Integer id);
 
-    @PostConstruct
-    public void init() {
-        runs.add(new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 5000, Location.OUTDOOR));
-        runs.add(new Run(2, "Evening Run", LocalDateTime.now(), LocalDateTime.now().plus(60, ChronoUnit.MINUTES), 3000, Location.INDOOR));
-        runs.add(new Run(3, "Night Run", LocalDateTime.now(), LocalDateTime.now().plus(40, ChronoUnit.MINUTES), 7000, Location.OUTDOOR));
-    }
+    int count();
+
+    void saveAll(List<Run> runs);
+
+    List<Run> findByLocation(String location);
 }
